d49801bc8067521e55918be26c5b2b446f54dcfb
Carnevale Stagisti <davide.fedeli@studenti.itisarezzo.it>
2023-02-01 12:33:16 +0100

    Algoritmo pazzo sgravato

--- maschera/registerMaschera.php
+++ maschera/registerMaschera.php
@@ -5,28 +5,30 @@ $deposito = $_POST['deposito'];
 $sarta = $_POST['sarta'];
 $descrizione = $_POST['descrizione'];
 $descrizione = trim($descrizione);
+$tmp = 0;
 
-$sql = "SELECT codMaschera FROM maschera order by codMaschera desc LIMIT 1";
+$sql = "SELECT codMaschera FROM maschera";
 $stmt = $conn->query($sql);
 $stmt->execute();
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    explode('S', $row['codMaschera']);
-    $row['codMaschera']++;    
-    $codMaschera = $row['codMaschera'];
+    $codMaschera = explode('MS', $row['codMaschera'])[1];
+    if($codMaschera > $tmp){
+        $tmp = $codMaschera;    
+    }   
 }
+$tmp++;
+$codMaschera = 'MS' . $tmp;
 
 
 $sql = "INSERT INTO maschera values
  ('$codMaschera', '$colore' , '$descrizione' , DEFAULT , '$sarta' , '$deposito');";
-echo $sql;
-
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 
 
-if (isset($_FILES['my_image'])) {
-
+if (isset($_FILES['my_image'])) {    
     $img_name = $_FILES['my_image']['name'];
+    echo $img_name;
     $img_size = $_FILES['my_image']['size'];
     $tmp_name = $_FILES['my_image']['tmp_name'];
     $error = $_FILES['my_image']['error'];
@@ -42,26 +44,29 @@ if (isset($_FILES['my_image'])) {
             $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
             $img_upload_path = '../uploads/' . $new_img_name;
             move_uploaded_file($tmp_name, $img_upload_path);
-            $sql = 'SELECT codFoto from galleria order by codFoto desc limit 1';
+            $sql = 'SELECT codFoto from galleria';
             $stmt = $conn->query($sql);            
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            explode('T' , $row['codFoto']);
-            $row['codFoto']++;
-            $codFoto = $row['codFoto'];            
+            foreach($stmt as $row) {                
+                $codFoto = explode('F', $row['codFoto'])[1];
+                if ($codFoto > $tmp) {
+                    $tmp = $codFoto;
+                }
+            }
+            $tmp++;
+            $codFoto = "FT".$tmp;            
             // Insert into Database
-            echo '<script>alert(' . $codMaschera . ')</script>';
             $sql = "INSERT INTO galleria
 				        VALUES('$codFoto' , '$new_img_name' , '$codMaschera')";
-
+            echo $sql;
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
-            header("Location: aggiungiMaschera.php");
+            //header("Location: aggiungiMaschera.php");
         } else {
             $em = "non puoi inserire file di questo tipo";
-            header("Location: aggiungiMaschera.php?error=$em");
+            //header("Location: aggiungiMaschera.php?error=$em");
         }
     }
 } else {
-   // header("Location: aggiungiMaschera.php");
+   //header("Location: aggiungiMaschera.php");
 }
